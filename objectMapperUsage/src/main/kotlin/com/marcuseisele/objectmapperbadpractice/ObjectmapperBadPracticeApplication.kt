5ca07f75688a7d179112a8b1327cce47fa55ee37
eiselems <marcus.eisele@gmail.com>
2020-02-21 23:05:02 +0100

    INIT

---
+++ objectMapperUsage/src/main/kotlin/com/marcuseisele/objectmapperbadpractice/ObjectmapperBadPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.marcuseisele.objectmapperbadpractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ObjectmapperBadPracticeApplication
+
+fun main(args: Array<String>) {
+	runApplication<ObjectmapperBadPracticeApplication>(*args)
+}
---
+++ objectMapperUsage/src/test/kotlin/com/marcuseisele/objectmapperbadpractice/ObjectmapperBadPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.marcuseisele.objectmapperbadpractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ObjectmapperBadPracticeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
